7e1bbcc4ce31a98e04b69dc2a0fbcdad6489dcf0
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2025-01-30 15:06:36 +0300

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,11 @@
 use serde::{Deserialize, Serialize};
 use chrono::NaiveDateTime;
+use diesel::{Queryable, Insertable};
+use crate::schema::samples;
+use crate::schema::users;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "samples"]
 pub struct Sample {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<i32>,
@@ -11,15 +15,16 @@ pub struct Sample {
     pub status: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct LoginRequest {
-    pub username: String,
-    pub password: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "users"]
 pub struct User {
     pub id: i32,
     pub username: String,
     pub password: String, // Store the hashed password
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LoginRequest {
+    pub username: String,
+    pub password: String,
 }
