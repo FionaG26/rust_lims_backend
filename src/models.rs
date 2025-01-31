f1efbdb85f962dc8af131b845375dd8a90a75ea0
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2025-01-31 18:38:40 +0300

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -4,6 +4,7 @@ use diesel::prelude::*;
 
 #[derive(Queryable, Serialize, Deserialize, Debug)]
 pub struct Sample {
+    #[serde(skip_deserializing)] // Skip the 'id' field during deserialization
     pub id: i32,
     pub name: String,
     pub sample_type: String,
